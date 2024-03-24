0c20ad8d992dceb1973291305f70a5e2636993a0
Colin Marc <hi@colinmarc.com>
2024-03-24 11:49:28 +0100

    feat(server): use a higher precision blend format

--- mm-server/src/compositor/video/composite.rs
+++ mm-server/src/compositor/video/composite.rs
@@ -12,7 +12,7 @@ use crate::vulkan::*;
 
 use super::SurfaceTexture;
 
-pub const BLEND_FORMAT: vk::Format = vk::Format::B8G8R8A8_SRGB;
+pub const BLEND_FORMAT: vk::Format = vk::Format::R16G16B16A16_SFLOAT;
 
 #[derive(Copy, Clone, Debug)]
 #[repr(C)]
