e9f0d18da517e1c7f1ab34d9c154b8ba70573f2e
Colin Marc <hi@colinmarc.com>
2024-05-05 13:00:23 +0200

    fix(mmclient): tune verbosity of conn message

--- mm-client/src/conn.rs
+++ mm-client/src/conn.rs
@@ -456,7 +456,7 @@ impl InnerConn {
             let (msg, len) = match protocol::decode_message(&buf) {
                 Ok(v) => v,
                 Err(protocol::ProtocolError::ShortBuffer(n)) => {
-                    debug!(sid, have = buf.len(), needed = n, "partial message");
+                    trace!(sid, have = buf.len(), needed = n, "partial message");
                     self.partial_reads.insert(sid, buf);
                     break;
                 }
